1a156c747d5ec2115e3cc3eb7cb5a3b98c518c39
Kelly Taylor <kajackdfw@yahoo.com>
2013-06-29 21:55:31 -0500

    new file

---
+++ controller/LoginController.js
@@ -0,0 +1,14 @@
+SeedApp.controller('LoginController', function ($scope, $location, $routeParams, UserModel ) {
+	
+	<!-- UI ACTIONS -->
+	
+	<!-- Login -->
+	$scope.login = function( email, password ) {
+		alert( email );
+		
+	};
+	
+
+	
+	
+});
