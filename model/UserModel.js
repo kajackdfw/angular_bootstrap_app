b8a3672849abe64a827dc47af374e525f7c2b20a
Kelly Taylor <kajackdfw@yahoo.com>
2013-06-29 21:53:16 -0500

    new file

---
+++ model/UserModel.js
@@ -0,0 +1,18 @@
+SeedApp.service('UserModel', function() {
+
+
+	this.login = function(user.email, user.password) {
+		
+		
+		
+
+		if (!user) {
+			user = { userId: "0", firstName: "", lastName: "", theme: "" }
+		}
+
+		user.push(user);
+		<!-- window.localStorage.setItem(user, JSON.stringify(user)); -->
+	};
+
+
+});
