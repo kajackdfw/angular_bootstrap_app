f3d12f35658bccc3bdf3ad31e1a8679018d95b9f
Kelly Taylor <kajackdfw@yahoo.com>
2013-10-15 21:23:14 -0500

    changes to prepare for mysql data

--- model/LinkModel.js
+++ model/LinkModel.js
@@ -1,32 +1,33 @@
 SeedApp.service('LinkModel', function() {
+	
 	this.getLinks = function() {
 
-                <!-- return $http.jsonp('http://localhost/rest/server.php/angularJS/technologies'); -->
+       <!-- return $http.jsonp('http://localhost/rest/server.php/angularJS/technologies'); -->
 
 		return [{
-				id: 1,
-				title: 'AngularJS Home',
-				url: 'https://angularjs.org'
+				technology_id: 1,
+				technology_title: 'AngularJS Home',
+				technology_url: 'https://angularjs.org'
 			}, {
-				id: 2,
-				title: 'Angular UI',
-				url: 'http://angular-ui.github.io/bootstrap/'
+				technology_id: 2,
+				technology_title: 'Angular UI',
+				technology_url: 'http://angular-ui.github.io/bootstrap/'
 			}, {
-				id: 3,
-				title: 'Plunker UI Code Tester',
-				url: 'http://plnkr.co/'
+				technology_id: 3,
+				technology_title: 'Plunker UI Code Tester',
+				technology_url: 'http://plnkr.co/'
 			}, {
-				id: 4,
-				title: 'Mongo Reference and/or Service',
-				url: 'https://mongolab.com/welcome/'
+				technology_id: 4,
+				technology_title: 'Mongo Reference and/or Service',
+				technology_url: 'https://mongolab.com/welcome/'
 			}, {
-				id: 5,
-				title: 'NodeJS Host',
-				url: 'https://www.nodejitsu.com'
+				technology_id: 5,
+				technology_title: 'NodeJS Host',
+				technology_url: 'https://www.nodejitsu.com'
 			}, {
-				id: 6,
-				title: 'Twitter Bootstrap',
-				url: 'http://twitter.github.io/bootstrap/index.html'
+				technology_id: 6,
+				technology_title: 'Twitter Bootstrap',
+				technology_url: 'http://twitter.github.io/bootstrap/index.html'
 			}
 		]};
-	});			
+	});
